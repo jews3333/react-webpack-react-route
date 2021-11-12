fedfae7b5495d3581248d5a2bf04f285a6aee17d
jews3333 <jews333333@gmail.com>
2021-11-12 17:17:59 +0900

    first commit

---
+++ webpack.config.js
@@ -0,0 +1,32 @@
+const path = require('path');
+
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+    entry: {
+        'js/app': [
+            './src/App.js'
+        ]
+    },
+    output: {
+        path: path.resolve(__dirname,'dist/'),
+        publicPath: '/'
+    },
+    module: {
+        rules: [
+            {
+                test: /\.(js|jsx)$/,
+                use: [
+                    'babel-loader'
+                ],
+                exclude: /node_modules/
+            }
+        ]
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+            template: './src/index.html',
+            filename: 'index.html'
+        })
+    ]
+}
