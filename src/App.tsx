5064e8fd13f8d0b2b3b2312dbc2dd57d9b4f1973
jews3333 <jews333333@gmail.com>
2021-11-17 16:30:55 +0900

    styled-components

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,14 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
+import * as React from 'react';
+import * as ReactDOM from 'react-dom';
+import styled from 'styled-components';
 
+const Title = styled.h1`
+    color:red;
+`;
 interface Props {}
 
 function App({}:Props){
-    return <h1>Hellow Webpack</h1>
+    return <Title>Hellow Webpack</Title>
 }
 
 ReactDOM.render(<App/>, document.getElementById('app'));
